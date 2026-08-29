0680eb4c8883d18e8e0383c952210a119412fd4e
Famiu Haque <famiu@famiu.dev>
2026-08-29 06:15:28 +0600

    Add profile header and bio UI

--- frames/Profile.java
+++ frames/Profile.java
@@ -1,26 +1,34 @@
 package frames;
 
 import java.lang.*;
+import java.io.*;
 import java.util.*;
 import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.text.*;
 import java.awt.*;
+import java.awt.event.*;
 
 import entity.*;
 import storage.*;
 
-public class Profile extends LibertaFrame {
+public class Profile extends LibertaFrame implements ActionListener {
     private String signedInUsername;
     private String profileUsername;
 
     private SidebarPanel sidebar;
     private FeedPanel feedPanel;
+    private JPanel headerPanel;
+
+    private LibertaButton editProfileButton;
+    private LibertaButton deleteAccountButton;
 
     public Profile(String signedInUsername) {
         this(signedInUsername, signedInUsername);
     }
 
     public Profile(String signedInUsername, String profileUsername) {
-        super(signedInUsername.equals(profileUsername) ? "My Profile" : profileUsername + "'s Profile", new BorderLayout());
+        super(getProfileTitle(signedInUsername, profileUsername), new BorderLayout());
 
         this.signedInUsername = signedInUsername;
         this.profileUsername = profileUsername;
@@ -33,13 +41,132 @@ public class Profile extends LibertaFrame {
             activeItem = "Home";
         }
 
-        // TODO(famiu): add the profile header, bio, and remaining actions after like and bio
-        // storage support is implemented.
         sidebar = new SidebarPanel(this, signedInUsername, activeItem);
         feedPanel = new FeedPanel(getProfilePosts(profileUsername), signedInUsername);
+        headerPanel = createHeaderPanel();
+
+        JPanel profilePanel = new JPanel(new BorderLayout());
+        profilePanel.setBackground(Theme.BACKGROUND);
+        profilePanel.add(headerPanel, BorderLayout.NORTH);
+        profilePanel.add(feedPanel, BorderLayout.CENTER);
 
         this.add(sidebar, BorderLayout.WEST);
-        this.add(feedPanel, BorderLayout.CENTER);
+        this.add(profilePanel, BorderLayout.CENTER);
+    }
+
+    private static String getProfileTitle(String signedInUsername, String profileUsername) {
+        if (signedInUsername.equals(profileUsername)) {
+            return "My Profile";
+        }
+        else {
+            return profileUsername + "'s Profile";
+        }
+    }
+
+    private JPanel createHeaderPanel() {
+        UserAccount profileUser = UserStorage.getUser(profileUsername);
+
+        JPanel headerPanel = new JPanel();
+        headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.Y_AXIS));
+        headerPanel.setBackground(Theme.BACKGROUND);
+
+        Border separator = BorderFactory.createMatteBorder(0, 0, 1, 0, Theme.BACKGROUND3);
+        Border padding = BorderFactory.createEmptyBorder(24, 24, 24, 24);
+        headerPanel.setBorder(BorderFactory.createCompoundBorder(separator, padding));
+
+        File profilePictureFile = UserStorage.getUserProfilePicture(profileUsername);
+        ImageIcon profilePicture = new ImageIcon(profilePictureFile.getPath());
+        Image scaledImage = profilePicture.getImage().getScaledInstance(128, 128, Image.SCALE_SMOOTH);
+        profilePicture.setImage(scaledImage);
+
+        JLabel profilePictureLabel = new JLabel(profilePicture);
+        profilePictureLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel displayNameLabel = new JLabel(profileUser.getDisplayName());
+        displayNameLabel.setFont(Theme.BOLD_FONT.deriveFont(24f));
+        displayNameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel usernameLabel = new JLabel("@" + profileUsername);
+        usernameLabel.setForeground(Theme.TEXT_MUTED);
+        usernameLabel.setFont(Theme.MAIN_FONT.deriveFont(16f));
+        usernameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        headerPanel.add(profilePictureLabel);
+        headerPanel.add(Box.createVerticalStrut(12));
+        headerPanel.add(displayNameLabel);
+        headerPanel.add(Box.createVerticalStrut(4));
+        headerPanel.add(usernameLabel);
+
+        String bio = profileUser.getBio();
+        if (!bio.isEmpty()) {
+            JTextPane bioPane = createBioPane(bio);
+
+            headerPanel.add(Box.createVerticalStrut(20));
+            headerPanel.add(bioPane);
+        }
+
+        if (signedInUsername.equals(profileUsername)) {
+            JPanel actionPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 12, 0));
+            actionPanel.setOpaque(false);
+
+            editProfileButton = new LibertaButton("Edit Profile");
+            editProfileButton.setColors(Theme.ACCENT1, Theme.ACCENT2, Theme.TEXT, Theme.TEXT, true);
+            editProfileButton.setMargin(new Insets(8, 16, 8, 16));
+            editProfileButton.addActionListener(this);
+
+            deleteAccountButton = new LibertaButton("Delete Account");
+            deleteAccountButton.setColors(Theme.DANGER, Theme.DANGER.brighter(), Theme.TEXT, Theme.TEXT, true);
+            deleteAccountButton.setMargin(new Insets(8, 16, 8, 16));
+            deleteAccountButton.addActionListener(this);
+
+            actionPanel.add(editProfileButton);
+            actionPanel.add(deleteAccountButton);
+
+            headerPanel.add(Box.createVerticalStrut(16));
+            headerPanel.add(actionPanel);
+        }
+
+        return headerPanel;
+    }
+
+    private static JTextPane createBioPane(String bio) {
+        JTextPane bioPane = new JTextPane();
+        bioPane.setText(bio);
+        bioPane.setEditable(false);
+        bioPane.setOpaque(false);
+        bioPane.setBorder(null);
+        bioPane.setFocusable(false);
+        bioPane.setFont(Theme.MAIN_FONT);
+        bioPane.setForeground(Theme.TEXT);
+
+        // JTextArea doesn't support center alignment, JLabel doesn't have line wrapping,
+        // so we have to use JTextPane with StyledDocument
+        // Reference: https://stackoverflow.com/questions/66440929/jtextarea-center-alignment
+        StyledDocument documentStyle = bioPane.getStyledDocument();
+        SimpleAttributeSet centerAttribute = new SimpleAttributeSet();
+        StyleConstants.setAlignment(centerAttribute, StyleConstants.ALIGN_CENTER);
+        documentStyle.setParagraphAttributes(0, documentStyle.getLength(), centerAttribute, false);
+
+        // JTextPane needs a fixed width to calculate preferred height for wrapped text, so we have
+        // to set its width before getting its preferred size, otherwise it will take more height
+        // than necessary and leave a lot of empty space below the bio
+        // Reference: https://stackoverflow.com/questions/39455573/how-to-set-fixed-width-but-dynamic-height-on-jtextpane
+        bioPane.setSize(new Dimension(600, 400));
+        Dimension bioSize = new Dimension(600, bioPane.getPreferredSize().height);
+        bioPane.setPreferredSize(bioSize);
+        bioPane.setMaximumSize(bioSize);
+        bioPane.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        return bioPane;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == editProfileButton) {
+            // TODO(famiu): open Edit Profile and persist valid changes
+        }
+        else if (e.getSource() == deleteAccountButton) {
+            // TODO(famiu): confirm and delete the signed-in user's account
+        }
     }
 
     // Get the displayed user's posts in reverse chronological order
@@ -48,7 +175,7 @@ public class Profile extends LibertaFrame {
         ArrayList<Post> profilePosts = new ArrayList<Post>();
 
         // Convert post IDs to Post objects
-        for (Integer postId : userPostIds) {
+        for (int postId : userPostIds) {
             Post post = PostStorage.getPost(postId);
             if (post != null) {
                 profilePosts.add(post);
